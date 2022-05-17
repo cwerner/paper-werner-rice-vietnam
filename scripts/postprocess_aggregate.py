ed7bb063d3083130bc9d696e948c7aa7d641d541
Christian Werner <christian.werner@kit.edu>
2022-05-17 09:24:37 +0200

    Update aggregation stage

--- scripts/postprocess_aggregate.py
+++ scripts/postprocess_aggregate.py
@@ -1,11 +1,14 @@
 import logging
 import sys
+import tempfile
 import warnings
 from functools import partial
+from io import StringIO
 from pathlib import Path
-from typing import Any, Iterable, Optional
+from typing import Any, Iterable, Optional, Union
 
 import numpy
+import pandas as pd
 import rich_click.typer as typer
 import xarray as xr
 from dask.distributed import Client
@@ -49,6 +52,25 @@ vars_seasonal = [
     "dN_n2o_emis",
 ]
 
+merge_table = pd.read_csv(
+    StringIO(
+        """regionid,region,name,irri,mixed
+        2,1,northern_mountains,0.6,0.4
+        7,1,northern_mountains,0.6,0.4
+        4,2,red_river_delta,0.0,1.0
+        5,3,northern_central,0.4,0.6
+        6,3,northern_central,0.4,0.6
+        8,4,central_highlands,0.6,0.4
+        3,5,east_southern_region,0.0,1.0
+        1,6,mekong_delta,1.0,0.0
+        """
+    )
+)
+
+
+def convert_strpath(path: Union[str, Path]) -> Path:
+    return path if isinstance(path, Path) else Path(path)
+
 
 class ProgressParallel(Parallel):
     def __init__(self, label: str, total: Optional[int] = None, *args, **kwargs):
@@ -90,6 +112,77 @@ def aggregate_seasonal(ncfile: Path, *, outdir: Path, vars: Iterable[str]) -> No
     return None
 
 
+def merge_by_merge_table(
+    ncfile1: Path,
+    ncfile2: Path,
+    *,
+    outdir: Path,
+    merge_table: pd.DataFrame,
+    ref: xr.DataArray,
+) -> None:
+
+    comp = dict(zlib=True, complevel=5)
+    encoding = {}
+
+    with xr.open_dataset(ncfile1) as ds1, xr.open_dataset(ncfile2) as ds2:
+        frac1 = ref.copy().where(ds1.surfacetemperature.isnull is False)
+        frac2 = ref.copy().where(ds2.surfacetemperature.isnull is False)
+
+        for _, row in merge_table.iterrows():
+            frac1 = frac1.where(frac1 == int(row["regionid"]), int(row["irri"]))
+            frac2 = frac2.where(frac2 == int(row["regionid"]), int(row["mixed"]))
+
+        dsout = ds1 * frac1 + ds2 * frac2
+        for var in dsout.data_vars:
+            encoding[var] = comp
+
+        dsout.to_netcdf(
+            outdir
+            / ncfile1.name.replace("_irrigated-", "_merged-").replace(
+                "_upland-", "_merged-"
+            ),
+            encoding=encoding,
+        )
+
+
+def process_merge_mana_scens(
+    workload1: Iterable[Any],
+    workload2: Iterable[Any],
+    *,
+    outdir: Union[str, Path],
+    ref: xr.DataArray,
+    cores: int,
+):
+    outdir = convert_strpath(outdir)
+
+    ftype = "seasonal" if "seasonal" in str(workload1[0]) else "yearly"
+
+    logger.debug(workload1)
+    logger.debug(workload2)
+
+    func = partial(
+        merge_by_merge_table, outdir=outdir, merge_table=merge_table, ref=ref
+    )
+    ProgressParallel(f"MERGE {ftype}", n_jobs=cores)(
+        delayed(func)(ncfile1, ncfile2)
+        for ncfile1, ncfile2 in zip(workload1, workload2)
+    )
+
+
+def process_seasonal(
+    workload: Iterable[Any],
+    *,
+    outdir: Union[str, Path],
+    vars: Iterable[str],
+    cores: int,
+):
+    outdir = convert_strpath(outdir)
+    func = partial(aggregate_seasonal, outdir=outdir, vars=vars)
+    ProgressParallel("AGG seasonal", n_jobs=cores)(
+        delayed(func)(ncfile) for ncfile in workload
+    )
+
+
 def aggregate_annual(ncfile: Path, *, outdir: Path, vars: Iterable[str]) -> None:
     comp = dict(zlib=True, complevel=5)
     encoding = {}
@@ -101,9 +194,7 @@ def aggregate_annual(ncfile: Path, *, outdir: Path, vars: Iterable[str]) -> None
         mean_vars = ["surfacewater", "surfacetemperature"]
         sum_vars = [x for x in ds.data_vars if x not in mean_vars]
 
-        sum_ds = (
-            ds[sum_vars].groupby("time.year").sum(dim="time")
-        )  # .to_netcdf(outdir / ncfile.name, encoding=encoding)
+        sum_ds = ds[sum_vars].groupby("time.year").sum(dim="time")
         mean_ds = ds[mean_vars].groupby("time.year").mean(dim="time")
         dsout = xr.merge([sum_ds, mean_ds])
         dsout.to_netcdf(
@@ -113,18 +204,15 @@ def aggregate_annual(ncfile: Path, *, outdir: Path, vars: Iterable[str]) -> None
     return None
 
 
-def process_seasonal(
-    workload: Iterable[Any], *, outdir: Path, vars: Iterable[str], cores: int
-):
-    func = partial(aggregate_seasonal, outdir=outdir, vars=vars)
-    ProgressParallel("AGG seasonal", n_jobs=cores)(
-        delayed(func)(ncfile) for ncfile in workload
-    )
-
-
 def process_annual(
-    workload: Iterable[Any], *, outdir: Path, vars: Iterable[str], cores: int
+    workload: Iterable[Any],
+    *,
+    outdir: Union[str, Path],
+    vars: Iterable[str],
+    cores: int,
 ):
+    outdir = convert_strpath(outdir)
+
     func = partial(aggregate_annual, outdir=outdir, vars=vars)
     ProgressParallel("AGG annual  ", n_jobs=cores)(
         delayed(func)(ncfile) for ncfile in workload
@@ -132,9 +220,9 @@ def process_annual(
 
 
 def calc_seasonal_pctl(
-    workload: Iterable[Any], *, outdir: Path, percentiles: Iterable[float]
+    workload: Iterable[Any], *, outdir: Union[str, Path], percentiles: Iterable[float]
 ):
-
+    outdir = convert_strpath(outdir)
     comp = dict(zlib=True, complevel=5)
     encoding = {}
 
@@ -171,9 +259,9 @@ def calc_seasonal_pctl(
 
 
 def calc_annual_pctl(
-    workload: Iterable[Any], *, outdir: Path, percentiles: Iterable[float]
+    workload: Iterable[Any], *, outdir: Union[str, Path], percentiles: Iterable[float]
 ):
-
+    outdir = convert_strpath(outdir)
     comp = dict(zlib=True, complevel=5)
     encoding = {}
 
@@ -210,6 +298,7 @@ def calc_annual_pctl(
 def main(
     indir: Path = typer.Argument(None, exists=True, file_okay=False),
     outdir: Path = typer.Argument(None, exists=True, file_okay=False),
+    reffile: Path = typer.Argument(None, exists=True, dir_okay=False),
     debug: bool = typer.Option(False, help="Print debugging output"),
     cores: int = typer.Option(cpu_count()),
 ):
@@ -229,29 +318,73 @@ def main(
                 "vars_seasonal": vars_seasonal,
             }
         )
-    logger.debug(f"Config: {config}")
 
-    workload = list(indir.glob("*.nc"))
+    logger.debug(f"Using {cores} cores")
+    logger.info("Stage 1: creating annual and seasonal source nc files")
 
-    logger.debug(f"Using {cores} cores for {len(workload)} files")
+    # use tmp dir for those files...
+    with tempfile.TemporaryDirectory(dir=Path.cwd()) as tmp:
+        ptmp = Path(tmp)
 
-    process_annual(workload, outdir=outdir, vars=config.vars_annual, cores=cores)
+        logger.debug(f"Using tmp folder {tmp}")
+        logger.info("Stage 1a: Fully irrigated management files")
 
-    process_seasonal(workload, outdir=outdir, vars=config.vars_seasonal, cores=cores)
-
-    with Client(silence_logs=logging.ERROR) as client:
-        logger.debug(
-            f"Using dask for upcoming aggregations. See {client.dashboard_link}"
+        workload = sorted(list(indir.glob("VN_arable_irrigated-*.nc")))
+        process_annual(workload, outdir=tmp, vars=list(config.vars_annual), cores=cores)
+        process_seasonal(
+            workload, outdir=tmp, vars=list(config.vars_seasonal), cores=cores
         )
 
+        logger.info("Stage 1b: Upland management files")
+
+        # workload = list(indir.glob("*VN_arable_upland_*.nc"))
+        # process_annual(workload, outdir=tmp, vars=config.vars_annual, cores=cores)
+        # process_seasonal(workload, outdir=tmp, vars=config.vars_seasonal, cores=cores)
+
+        # fake files
+        import shutil
+
+        [
+            shutil.copyfile(f, ptmp / str(f.name).replace("irrigated-", "upland-"))
+            for f in ptmp.glob("*irrigated*.nc")
+        ]
+
+        logger.info("Stage 2: merge scenarios based on merge table")
+
+        ref_da: xr.DataArray = xr.open_dataset(reffile)["regionid"].load()
+
+        workload1 = sorted(list(ptmp.glob("*irrigated*_yearly.nc")))
+        workload2 = sorted(list(ptmp.glob("*upland*_yearly.nc")))
+        process_merge_mana_scens(
+            workload1, workload2, outdir=tmp, ref=ref_da, cores=cores
+        )
+
+        workload1 = sorted(list(ptmp.glob("*irrigated*_seasonal.nc")))
+        workload2 = sorted(list(ptmp.glob("*upland*_seasonal.nc")))
+        process_merge_mana_scens(
+            workload1, workload2, outdir=tmp, ref=ref_da, cores=cores
+        )
+
+        logger.info("Stage 3: creating annual and seasonal percentile aggregations")
         pctl = [0.05, 0.25, 0.50, 0.75, 0.95]
 
-        workload = list(outdir.glob("*seasonal.nc"))
-        calc_seasonal_pctl(workload, outdir=outdir, percentiles=pctl)
+        with Client(silence_logs=logging.ERROR) as client:
+            logger.debug(f"Using dask during aggregations. See {client.dashboard_link}")
 
-        workload = list(outdir.glob("*yearly.nc"))
-        calc_annual_pctl(workload, outdir=outdir, percentiles=pctl)
+            workload = sorted(list(ptmp.glob("*merged*seasonal.nc")))
+            calc_seasonal_pctl(workload, outdir=outdir, percentiles=pctl)
+
+            workload = sorted(list(ptmp.glob("*merged*yearly.nc")))
+            calc_annual_pctl(workload, outdir=outdir, percentiles=pctl)
 
 
 if __name__ == "__main__":
     typer.run(main)
+
+    # ref_da = xr.open_dataset("data/raw/misc/VN_MISC5_V2.nc")['regionid'].load()
+    # tmp = "tmp"
+    # ptmp = Path(tmp)
+    # cores=8
+    # workload1 = sorted(list(ptmp.glob("*irrigated*_yearly.nc")))
+    # workload2 = sorted(list(ptmp.glob("*upland*_yearly.nc")))
+    # process_merge_mana_scens(workload1, workload2, outdir=tmp, ref=ref_da, cores=cores)
